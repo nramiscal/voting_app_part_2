4b3bcd9136718ad01bfe26b3c77261e569c99e4d
nramiscal <nramiscal@gmail.com>
2019-04-02 22:40:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Library extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          name: "React",
+          value: 0,
+      }
+  }
+
+  increment() {
+      this.setState({value: this.state.value + 1});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <div className="library">
+            <div className="circle">{this.state.value}</div>
+            <h1 className="value">{this.state.name}</h1>
+            <div className='plusSign' onClick = {()=>{this.increment()}}>
+              <div className="bar horizontal"></div>
+              <div className="bar vertical"></div></div>
+          </div>
     );
   }
+}
+
+
+let libraryId = 4;
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            libraries: [0,1,2,3]
+        }
+    }
+
+    addLibrary = () => {
+        let libraries = [...this.state.libraries];
+        libraries.push(libraryId);
+        libraryId++;
+        this.setState({ libraries: libraries });
+    }
+
+    render () {
+        const { libraries } = this.state;
+        return (
+            <div className="main">
+                <div className="header">
+                    <h1>Vote for Your JS Library</h1>
+                </div>
+                <div className="library-list">
+                    { libraries.map((id) => <Library key={id}/>) }
+                </div>
+
+            </div>
+        )
+    }
 }
 
 export default App;
